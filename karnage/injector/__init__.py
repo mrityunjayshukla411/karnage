7c9ca6686d4c9f031bc8cdd46d444d804b35c05c
mrityunjayshukla411 <mrityunjayshukla411@gmail.com>
2026-05-28 11:36:15 +0530

    feat: the injector module dumps the adjacency list

---
+++ karnage/injector/__init__.py
@@ -0,0 +1,19 @@
+from .injector import (
+    OpcodeInfo,
+    FlipInfo,
+    AdjacencyEntry,
+    load_matcher_table,
+    find_adjacent_pairs,
+    build_report,
+    run,
+)
+
+__all__ = [
+    "OpcodeInfo",
+    "FlipInfo",
+    "AdjacencyEntry",
+    "load_matcher_table",
+    "find_adjacent_pairs",
+    "build_report",
+    "run",
+]
